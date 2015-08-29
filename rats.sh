39df373e324679448514d7a41eef3c70552e9547
Alban Andrieu <alban.andrieu@free.fr>
2015-08-29 22:57:53 +0200

    OK

--- rats.sh
+++ rats.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+rats --version
+
 reports_directory="rats-reports"
 report_filename="rats-result-SAMPLE"
 if [ ! -e ${reports_directory} ]; then
     mkdir ${reports_directory}
 fi
+
 rats -w 3 --xml sample/microsoft > ${report_filename}.xml
 
 rats --warning 1 --html sample/microsoft > ${report_filename}.html
