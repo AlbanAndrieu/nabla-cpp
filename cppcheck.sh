4bc2b2c8598f8bf52329a7841a5ac99772841669
AlbanAndrieu <alban.andrieu@free.fr>
2015-08-29 22:57:53 +0200

    OK

--- cppcheck.sh
+++ cppcheck.sh
@@ -2,4 +2,12 @@
 
 cppcheck --version
 
-cppcheck --enable=all --inconclusive --xml --xml-version=2 sample/microsoft 2> cppcheck.xml
+reports_directory="cppcheck-reports"
+report_filename="cppcheck-result-SAMPLE"
+if [ ! -e ${reports_directory} ]; then
+    mkdir ${reports_directory}
+fi
+
+cppcheck --enable=all --inconclusive --xml --xml-version=2 sample/microsoft 2> ${report_filename}.xml
+
+mv ${report_filename}* ${reports_directory}
