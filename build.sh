d91f16eb34a166ba0c18ee3da3f5e132e6d3cc39
Andrieu, Alban <alban.andrieu@free.fr>
2017-02-10 18:45:38 +0100

    Cleaning

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ rm -Rf sample/build-linux/Makefile
 rm -Rf sample/build-linux/_CPack_Packages/
 rm -Rf nabla-*
 #rm -Rf nabla-*.tar.gz
-rm -Rf buildcache-*
-rm -Rf scons-signatures-*.dblite
+#rm -Rf buildcache-*
+#rm -Rf scons-signatures-*.dblite
 
 #scons opt=True
 scons target=local gcc_version=5 package 2>&1 > scons.log
