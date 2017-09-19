91ca460b2e59469e2ca18e9beb3e2597c3d9ef5f
AlbanAndrieu <alban.andrieu@free.fr>
2017-09-19 22:46:35 +0200

    PROJECT_TARGET_PATH to WORKSPACE/target

--- sample/build-linux/build.sh
+++ sample/build-linux/build.sh
@@ -7,6 +7,8 @@ echo "WORKSPACE ${WORKSPACE}"
 #export PROJECT_SRC=${WORKSPACE}
 #export PROJECT_TARGET_PATH=/target
 
+export PROJECT_TARGET_PATH=${WORKSPACE}/target
+
 echo "PROJECT_SRC : $PROJECT_SRC - PROJECT_TARGET_PATH : $PROJECT_TARGET_PATH"
 
 cd $PROJECT_SRC/sample/build-${ARCH}
@@ -14,7 +16,10 @@ cd $PROJECT_SRC/sample/build-${ARCH}
 rm -f CMakeCache.txt
 
 #-DCMAKE_C_COMPILER=i686-pc-cygwin-gcc-3.4.4 -DCMAKE_CXX_COMPILER=i686-pc-cygwin-g++-3
-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE -DCMAKE_INSTALL_PREFIX=${PROJECT_TARGET_PATH} ../microsoft
+#-DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ 
+#-DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE
+#-DCMAKE_INSTALL_PREFIX=${PROJECT_TARGET_PATH}
+cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=debug -DCMAKE_INSTALL_PREFIX=$PROJECT_SRC/install/${MACHINE}/debug ../microsoft
 #-DCMAKE_INSTALL_PREFIX=${PROJECT_TARGET_PATH}/install/${MACHINE}/debug
 #-DENABLE_TESTING=true
 
@@ -24,16 +29,23 @@ cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=debug -DCMAKE_EXPORT_
 #clang-tidy -dump-config
 
 /workspace/build-wrapper-linux-x86/build-wrapper-linux-x86-32 --out-dir ${WORKSPACE}/bw-outputs make -B clean install test DoxygenDoc package
+#~/build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir ${WORKSPACE}/bw-outputs make -B clean install DoxygenDoc
 
-#make Experimental
-
-#make test_all
-#cd src
 #ctest -D Experimental
 #cd ${WORKSPACE}/sample/build-linux/src/test/cpp
 #ctest .. -R circular_queueTest
-cd src/test/cpp/
+cd src/test/app/
 ctest --force-new-ctest-process --no-compress-output -T Test -O Test.xml || /bin/true
+
+#ctest -j4 -DCTEST_MEMORYCHECK_COMMAND="/usr/bin/valgrind" -DMemoryCheckCommand="/usr/bin/valgrind" --output-on-failure -T memcheck
+#ctest -T memcheck
+
+#cd $PROJECT_SRC/sample/build-${ARCH}
+make package
+
+make tests
+
+make Experimental
 
 #http://clang-analyzer.llvm.org/installation.html
 #http://clang-analyzer.llvm.org/scan-build.html
--- sample/build-linux/cmake.sh
+++ sample/build-linux/cmake.sh
@@ -5,5 +5,6 @@
 #-DCMAKE_C_COMPILER=i686-pc-cygwin-gcc-3.4.4 -DCMAKE_CXX_COMPILER=i686-pc-cygwin-g++-3
 #-DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++
 #-DCMAKE_INSTALL_PREFIX=/target/install/${ARCH}/debug
+#-DECLIPSE_CDT4_GENERATE_SOURCE_PROJECT=TRUE
 
-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DECLIPSE_CDT4_GENERATE_SOURCE_PROJECT=TRUE ../microsoft
+cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../microsoft
