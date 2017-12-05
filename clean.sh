02aacaac87efa52be09de8ec1254b0300033dfb5
AlbanAndrieu <alban.andrieu@free.fr>
2017-12-05 16:23:38 +0100

    Improvement

--- clean.sh
+++ clean.sh
@@ -9,6 +9,7 @@ rm -f DartConfiguration.tcl
 
 find . -name 'target' -type d | xargs rm -Rf
 find . -name 'CMakeFiles' -type d | xargs rm -Rf
+find . -name '*.cpp.*r.*' -type f | xargs rm -Rf
 rm -Rf sample/build-linux/MICROSOFT-10.02-Linux*
 rm -Rf sample/build-linux/Makefile
 rm -Rf sample/build-linux/_CPack_Packages/
