d6bd443df8233ea32b2127591c06245038789985
AlbanAndrieu <alban.andrieu@free.fr>
2018-01-08 12:33:52 +0100

    Add AddressSanitizer

--- clean.sh
+++ clean.sh
@@ -8,15 +8,21 @@ echo -e "${red} ${double_arrow} Cleaning started ${NC}"
 rm -f DartConfiguration.tcl
 
 find . -name 'target' -type d | xargs rm -Rf
-find . -name 'CMakeFiles' -type d | xargs rm -Rf
 find . -name '*.cpp.*r.*' -type f | xargs rm -Rf
+#Coverage
+find . -name '*.gcno' -type f | xargs rm -Rf
+find . -name '*.gcna' -type f | xargs rm -Rf
+#CMake
+find . -name 'CMakeFiles' -type d | xargs rm -Rf
 rm -Rf sample/build-linux/MICROSOFT-10.02-Linux*
 rm -Rf sample/build-linux/Makefile
 rm -Rf sample/build-linux/_CPack_Packages/
-rm -Rf nabla-*
-#rm -Rf nabla-*.tar.gz
+#Scons
 #rm -Rf buildcache-*
 #rm -Rf scons-signatures-*.dblite
+#Other
+rm -Rf nabla-*
+#rm -Rf nabla-*.tar.gz
 rm -Rf Nabla*
 rm -Rf target
 rm -Rf bin
