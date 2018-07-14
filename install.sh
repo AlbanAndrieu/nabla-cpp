8157943d3c1adc5061b1536fd531b3f9a041c4c0
AlbanAndrieu <alban.andrieu@free.fr>
2018-07-14 15:52:34 +0200

    Add clang-build

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ sudo apt-get install rats
 #sudo apt-get install findbugs
 sudo apt-get install flawfinder
 #sudo apt-get install cppncss complexity
-sudo apt-get install clang flawfinder cppcheck ggcov gcovr gperf doxygen
+sudo apt-get install clang clang-tools
+sudo apt-get install flawfinder cppcheck ggcov gcovr gperf doxygen
 sudo apt-get install ninja-build
 
 sudo apt-get install texlive-latex-extra texlive-xetex
